edb2e2ebcd6b110063aec4d119ca8e2cfee30211
SeaEclipse <xentleaker@protonmail.com>
2020-04-25 11:16:59 +0200

    Add files via upload

---
+++ flameCordPatchToTravertine.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+read -p 'Input file to patch -> ' originFile
+
+read -p 'Patch everything or just FlameCord and Bungee-specific classes?(everything/specific) -> ' everspec
+
+if [[ ""$everspec"" = everything ]]; then
+  jar uf $originFile net/*
+  jar uf $originFile com/*
+  jar uf $originFile gnu/*
+  jar uf $originFile io/*
+  jar uf $originFile joptsimple/*
+  jar uf $originFile META-INF/*
+  jar uf $originFile mojang-translations/*
+  jar uf $originFile org/*
+elif [[ ""$everspec"" = specific ]]; then
+  jar uf $originFile net/*
+  jar uf $originFile com/*
+  jar uf $originFile META-INF/*
+fi
